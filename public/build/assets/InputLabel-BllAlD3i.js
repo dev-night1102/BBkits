bafc55916469d46fc9726a704ba08a4d48e7287e
hunterdev90 <Hunterdev90@outlook.com>
2025-09-15 12:37:13 -0700

    fix: also

--- public/build/assets/InputLabel-BllAlD3i.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-Bcc7zUSL.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
